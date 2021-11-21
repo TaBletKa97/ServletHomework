9630b4f65e792fdb46b123686de852055e7936da
TaBletKa97 <mtabletka.by@gmail.com>
2021-11-21 20:35:14 +0300

    Start

---
+++ src/main/java/com/khatkevich/MainServlet.java
@@ -0,0 +1,20 @@
+package com.khatkevich;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println("<h1>My first servlet</h1>");
+        writer.println("</body>");
+        writer.println("<html>");
+    }
+}
